97006d27e7e1a1da7056bdb369be5316dd66835a
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-05 05:33:01 -0300

    calling backend via axios and starting to using hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import api from './services/api';
+
+import logo from './assets/Omni.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    console.log(response);
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <img src={logo} alt="Omnistack App Y"/>
+      <div className="content">
         <p>
-          Omnistack App - Y
+          Find spots to program around the world
         </p>
-      </header>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL</label>
+          <input
+            id="email"
+            type="email"
+            placeholer= "type your email here"
+            onChange={event => setEmail(event.target.value)}
+          />
+          <button className="btn" type="submit">Enter</button>
+        </form>
+      </div>
     </div>
   );
 }
