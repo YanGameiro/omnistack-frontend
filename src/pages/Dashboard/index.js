d4ca06eee7604e57439a2875ab85a82aacf9c204
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-05 07:03:01 -0300

    listing in dashboard all spots that one user had created

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 
 const Dashboard = () => {
-    return <div />
+    const [spots, setSpots] = useState([]);
+
+    useEffect(()=>{
+        const loadSpots = async () => {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+        }
+        loadSpots();
+    },[]);
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default Dashboard;
