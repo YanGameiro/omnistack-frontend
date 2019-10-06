7f26510d0a3db298bb4021b044397e6f426db4c7
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-06 19:19:50 -0300

    showing new requests via socketio

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import socketio from 'socket.io-client';
 
@@ -7,16 +7,19 @@ import './style.css'
 
 const Dashboard = () => {
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+    const socket = useMemo(() => socketio('http://192.168.0.9:3333', {
+        query: { user_id },
+    }), [user_id]);
 
     useEffect(() => {
-        const user_id = localStorage.getItem('user');
-        const socket = socketio('http://192.168.0.9:3333', {
-            query: { user_id },
-        });
+        
         socket.on('booking_request', data => {
-            console.log(data);
+            setRequests([...requests, data]);
         });
-    }, []);
+    }, [requests, socket]);
 
     useEffect(()=>{
         const loadSpots = async () => {
@@ -31,6 +34,19 @@ const Dashboard = () => {
     },[]);
     return (
         <>
+            <ul className="notifications">
+                {requests.map(request => (
+                    <li key={request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong>
+                             wants to go to <strong>{request.spot.company}</strong>
+                             on <strong>{request.date}</strong>
+                        </p>
+                        <button className="accept">ACCEPT</button>
+                        <button className="reject">REJECT</button>
+                    </li>
+                ))}
+            </ul>
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
